4bef81b455d3ffc005d94ace63587c8559e095ab
Alexey Tourbin <alexey.tourbin@gmail.com>
2017-09-12 04:34:40 +0300

    inline reeda() and peeka() when enough bytes in fda->buf
    This will also propagate constant size and eliminate memcpy()
    function call.

--- reada.c
+++ reada.c
@@ -5,26 +5,18 @@
 #include <sys/uio.h>
 #include "reada.h"
 
-ssize_t reada(struct fda *fda, void *buf, size_t size)
+ssize_t reada_(struct fda *fda, void *buf, size_t size, size_t left)
 {
-    assert(size > 0);
-
     size_t total = 0;
 
-    size_t left = fda->fill - fda->off;
     if (left) {
-	if (size < left) {
-	    memcpy(buf, fda->buf + fda->off, size);
-	    fda->off += size;
-	    return size;
-	}
 	memcpy(buf, fda->buf + fda->off, left);
 	size -= left, buf = (char *) buf + left;
 	fda->off = fda->fill = 0;
 	total += left;
     }
 
-    while (size) {
+    do {
 	size_t endpos = (fda->fpos + size + NREADA) / 4096 * 4096;
 	size_t asize = endpos - (fda->fpos + size);
 	assert(asize > NREADA - 4096);
@@ -48,39 +40,36 @@ ssize_t reada(struct fda *fda, void *buf, size_t size)
 	}
 	size -= n, buf = (char *) buf + n;
 	total += n;
-    }
+    } while (size);
 
     return total;
 }
 
-ssize_t peeka(struct fda *fda, void *buf, size_t size)
+ssize_t peeka_(struct fda *fda, void *buf, size_t size, size_t left)
 {
-    assert(size > 0);
     assert(size <= NREADA);
 
-    size_t left = fda->fill - fda->off;
-    if (left < size) {
-	if (fda->off) {
-	    memmove(fda->buf, fda->buf + fda->off, left);
-	    fda->off = 0, fda->fill = left;
-	}
-	do {
-	    size_t endpos = (fda->fpos + NREADA - fda->fill) / 4096 * 4096;
-	    size_t asize = endpos - fda->fpos;
-	    assert(asize > 0);
-	    assert(fda->fill + asize <= NREADA);
-	    ssize_t n;
-	    do
-		n = read(fda->fd, fda->buf + fda->fill, asize);
-	    while (n < 0 && errno == EINTR);
-	    if (n < 0)
-		return n;
-	    if (n == 0)
-		break;
-	    fda->fpos += n;
-	    fda->fill += n, left += n;
-	} while (left < size);
+    if (fda->off) {
+	memmove(fda->buf, fda->buf + fda->off, left);
+	fda->off = 0, fda->fill = left;
     }
+
+    do {
+	size_t endpos = (fda->fpos + NREADA - fda->fill) / 4096 * 4096;
+	size_t asize = endpos - fda->fpos;
+	assert(asize > 0);
+	assert(fda->fill + asize <= NREADA);
+	ssize_t n;
+	do
+	    n = read(fda->fd, fda->buf + fda->fill, asize);
+	while (n < 0 && errno == EINTR);
+	if (n < 0)
+	    return n;
+	if (n == 0)
+	    break;
+	fda->fpos += n;
+	fda->fill += n, left += n;
+    } while (left < size);
 
     if (left < size)
 	size = left;
--- reada.h
+++ reada.h
@@ -1,7 +1,9 @@
 #ifndef READA_H
 #define READA_H
 
+#include <assert.h>
 #include <sys/types.h> // ssize_t
+#include <string.h>
 
 // Raw buffer to be declared separately, no need to initialize.
 #define NREADA 8192
@@ -19,8 +21,40 @@ struct fda {
 extern "C" {
 #endif
 
-ssize_t reada(struct fda *fda, void *buf, size_t size);
-ssize_t peeka(struct fda *fda, void *buf, size_t size);
+ssize_t reada_(struct fda *fda, void *buf, size_t size, size_t left);
+ssize_t peeka_(struct fda *fda, void *buf, size_t size, size_t left);
+
+static inline
+ssize_t reada(struct fda *fda, void *buf, size_t size)
+{
+    assert(size > 0);
+
+    size_t left = fda->fill - fda->off;
+    if (left >= size) {
+	memcpy(buf, fda->buf + fda->off, size);
+	if (left == size)
+	    fda->off = fda->fill = 0;
+	else
+	    fda->off += size;
+	return size;
+    }
+
+    return reada_(fda, buf, size, left);
+}
+
+static inline
+ssize_t peeka(struct fda *fda, void *buf, size_t size)
+{
+    assert(size > 0);
+
+    size_t left = fda->fill - fda->off;
+    if (left >= size) {
+	memcpy(buf, fda->buf + fda->off, size);
+	return size;
+    }
+
+    return peeka_(fda, buf, size, left);
+}
 
 #ifdef __cplusplus
 }
